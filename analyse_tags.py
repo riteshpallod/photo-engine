93c9dd07a48de4ab089db9f1c40406aa4b46fafd
Ritesh Pallod <riteshpallod@gmail.com>
2018-03-31 22:21:46 +0530

    object detection code done

--- analyse_tags.py
+++ analyse_tags.py
@@ -4,6 +4,13 @@ class Tags:
 
     tags_shelf = shelve.open("tags_db.dat")
     
+    def setTagData(self, tag, data):
+        if type(data) is not list:
+            data = [data] 
+        self.tags_shelf[tag] += data #appending information
+        #self.tags_shelf[tag] = list(set(self.tags_shelf[tag])) #for unique tags
+
+
     def getTagData(self, tags):
         tag_data = []
         for tag in tags:
@@ -19,4 +26,6 @@ class Tags:
 if __name__ == "__main__":
     tag_link = Tags()
     tag_link.setupHardcodedInfo()
+    print(tag_link.getTagData(['car', 'cap']))  
+    tag_link.setTagData('car',[9999999, 1, 2, 121411])
     print(tag_link.getTagData(['car', 'cap']))        
